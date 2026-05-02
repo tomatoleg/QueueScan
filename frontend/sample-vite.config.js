95131da5b418a96f077b6346686b7f4569f38805
Tomatoleg <tomatoleg@hotmail.com>
2026-05-02 17:26:34 -0400

    Added sample config files

---
+++ frontend/sample-vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  server: {
+    host: true,
+
+    allowedHosts: [
+      "host.tailxxxxxx.ts.net",
+    ],
+
+    proxy: {
+      "/api": {
+        target: "http://backend-dev:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+
+      "/ws": {
+        target: "ws://backend-dev:8080",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+});
